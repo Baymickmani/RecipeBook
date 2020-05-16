288cc3103a5fa161da54a0efdd96764212fe12ae
CHUNDURU B M SIVA NAGA SAI <chbmsivanagasai@gmail.com>
2020-05-16 16:39:41 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyDTEW77Z9YYlUUG3p6RFNrcY2StAtvmags'
+  firebaseAPIKey: 'AbIazyamSiycDkTEW77Z9YYlUUG3p6RFNrcY2StAtvmags'
 };
